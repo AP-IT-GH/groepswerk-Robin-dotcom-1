26a40364e741d71fbd60c49b77e5dd54e811db81
Levi Noeninckx <levi.noeninckx@gmail.com>
2022-05-17 12:13:45 +0200

    change transform to rigidbody + add heuristic + add rotation

--- VR-Project/Assets/Scripts/GolfAgent.cs
+++ VR-Project/Assets/Scripts/GolfAgent.cs
@@ -10,30 +10,67 @@ public class GolfAgent : Agent
 {
     private Vector3 startingPos;
     private float speed;
+    //private float rotationSpeed;
+    private Rigidbody rb;
+    private bool didHit;
+    private int swings;
 
     public Transform holePos;
     public override void Initialize()
     {
         startingPos = transform.localPosition;
-        speed = 10f;
+        speed = 60f;
+        //rotationSpeed = 10f;
+        rb = GetComponent<Rigidbody>();
+        swings = 0;
+        didHit = false;
     }
 
     public override void OnEpisodeBegin()
     {
         transform.localPosition = startingPos;
+        rb.velocity = Vector3.zero;
+        didHit = false;
+        swings = 0;
     }
 
     public override void OnActionReceived(ActionBuffers actions)
     {
         float move;
+        //float rotate;
         move = actions.ContinuousActions[0];
-        transform.Translate(move * Vector3.right * speed * Time.deltaTime);
+        //rotate = actions.ContinuousActions[1];
+        //transform.Translate(move * Vector3.right * speed * Time.deltaTime);
+        rb.AddForce(transform.right * speed * move,ForceMode.Impulse);
+        //transform.RotateAround(transform.position,transform.up,Time.deltaTime * rotationSpeed * rotate);
 
        if (transform.localPosition.y < 0f)
        {
            SetReward(-1f);
            EndEpisode();
        }
+
+       if (swings >= 7)
+       {
+           SetReward(-1f);
+           EndEpisode();
+       }
+    }
+
+    private void Update()
+    {
+        if (didHit && !speed.Equals(0f))
+        {
+            swings += 1;
+            speed = 0f;
+            Debug.Log(swings);
+        }
+
+        if (rb.velocity == Vector3.zero)
+        {
+            didHit = false;
+            speed = 60f;
+        }
     }
 
     public override void CollectObservations(VectorSensor sensor)
@@ -41,10 +78,22 @@ public class GolfAgent : Agent
         sensor.AddObservation(holePos.localPosition);
     }
 
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var continiousActionsOut = actionsOut.ContinuousActions;
+        continiousActionsOut[0] = Input.GetKey(KeyCode.Space) ? 1.0f : 0f;
+
+        if (continiousActionsOut[0] == 1.0f)
+        {
+            didHit = true;
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("hole"))
         {
+            rb.velocity = Vector3.zero;
             SetReward(1f);
             EndEpisode();
         }
